620a81081b8710c8609d3045fbd9a7565dd3dfed
Tom D <tomd200@mac.com>
2017-09-27 14:55:30 +0000

    day 13

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// JavaScript File
+
+angular.module('TodoApp', ['ngRoute', 'RouteControllers']); // these are dependencies
+
+angular.module('TodoApp').config(function
+    ($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true); 
+
+// define routes:
+// this is substituting text within the url - making them look more logical and easier to read. 
+// the website appears to have a logical structure. 
+
+    $routeProvider
+    .when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+    })
+    .when('/accounts/register',{
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterController'
+    });
+// can add extra routes using .when (remember the ;)    
+});
+
